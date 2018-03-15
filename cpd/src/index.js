30341f6d541c6e9644a6fa0b7f616938c9d2e2fb
Strygyr <dorsey2@uwm.edu>
2018-03-15 11:14:59 -0500

    Updateed the HTML and CSS for an initial push

--- cpd/src/index.js
+++ cpd/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './mainpage.css';
+import App from './SpotifyPlayer';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
 registerServiceWorker();
